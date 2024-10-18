2329340bba23c86acf04051e8984c5c9408ebdf6
Rasim-1 <husanbaevrasim315@gmail.com>
2024-10-18 19:06:42 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Card from './conpanents/Card/Card'
+import './App.css'
+
+
+const App = () => {
+  return (
+  <>
+  <div className="wrapp">
+  <Card/>
+
+  </div>
+
+
+  </>
+  )
+}
+
+
+export default App
